776bbfdf36700dac27fe5986e4a533e226099b2d
Shahid Karimi <karimi.shahid@gmail.com>
2019-10-15 19:05:40 +0500

    Create MyClass.php

---
+++ MyClass.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\MyApplication
+
+class MyClass {
+
+    private $sytemUrl = null;
+    
+    public function testFuction( $url )
+    {
+        $this->systeemUrlr = $url;
+    }
+    
+    public function transformData( $rows )
+    {
+        foreach ($rows as $row) {
+            // Logic here
+        }
+    }
+}
